a6402024fe34851c16516a2f6bdda9e2f12955e8
KuznetsovMA <jztenk@gmail.com>
2020-11-23 00:21:37 +0300

    [Dev] Banner text-overflow fix

--- pages/styles.js
+++ pages/styles.js
@@ -1,7 +1,7 @@
 import { keyframes } from "@emotion/core"
 import styled from "@emotion/styled"
 
-const pulse1 = keyframes`
+const pulseForeground1 = keyframes`
     0%,
     16.667%,
     100% {
@@ -14,7 +14,7 @@ const pulse1 = keyframes`
     }
 `
 
-const pulse2 = keyframes`
+const pulseForeground2 = keyframes`
     0%, 100% {
       opacity: 0;
     }
@@ -30,7 +30,7 @@ const pulse2 = keyframes`
     }
 `
 
-const pulse3 = keyframes`
+const pulseForeground3 = keyframes`
   0%,
   50%,
   100% {
@@ -40,6 +40,48 @@ const pulse3 = keyframes`
   66.667%,
   83.333% {
     opacity: 1;
+  }
+`
+
+const pulseBackground1 = keyframes`
+    0%,
+    16.667%,
+    100% {
+        opacity: 0;
+    }
+
+    33.333%,
+    83.333% {
+        opacity: 1;
+    }
+`
+
+const pulseBackground2 = keyframes`
+    0%, 100% {
+      opacity: 1;
+    }
+
+    33.333%,
+    50% {
+      opacity: 0;
+    }
+
+    16.667%,
+    66.667% {
+      opacity: 1;
+    }
+`
+
+const pulseBackground3 = keyframes`
+  0%,
+  50%,
+  100% {
+    opacity: 1;
+  }
+
+  66.667%,
+  83.333% {
+    opacity: 0;
   }
 `
 
@@ -58,7 +100,7 @@ export const HeadlineStyled = styled.h2`
     -webkit-background-clip: text;
     background-clip: text;
     background-image: linear-gradient(90deg, #7928ca, #ff0080);
-    animation-name: ${pulse2};
+    animation-name: ${pulseForeground2};
     animation-duration: 8s;
     animation-iteration-count: infinite;
   }
@@ -75,6 +117,10 @@ export const HeadlineStyled = styled.h2`
     left: 0;
     z-index: 0;
 
+    animation-name: ${pulseBackground2};
+    animation-duration: 8s;
+    animation-iteration-count: infinite;
+
     .dark-mode & {
       color: #fff;
     }
@@ -82,15 +128,23 @@ export const HeadlineStyled = styled.h2`
 
   &:first-of-type {
     span {
-      animation-name: ${pulse1};
+      animation-name: ${pulseForeground1};
       background-image: linear-gradient(90deg, #007cf0, #00dfd8);
+    }
+
+    &:before {
+      animation-name: ${pulseBackground1};
     }
   }
 
   &:last-of-type {
     span {
-      animation-name: ${pulse3};
+      animation-name: ${pulseForeground3};
       background-image: linear-gradient(90deg, #ff4d4d, #f9cb28);
+    }
+
+    &:before {
+      animation-name: ${pulseBackground3};
     }
   }
 `
