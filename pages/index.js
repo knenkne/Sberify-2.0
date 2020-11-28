1872c3706480ad8b9a4250c5a7f1dbe3c40dc922
KuznetsovMA <jztenk@gmail.com>
2020-11-28 15:46:08 +0300

    [Dev] paragraph component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout"
 import Wrapper from "../components/wrapper"
 import Headline from "../components/headline"
 import Button from "../components/button"
+import Paragraph from "../components/paragraph"
 
 const titles = ["Explore.", "Listen.", "Share."]
 
@@ -13,6 +14,12 @@ const Home = () => (
       ))}
       <Button filled>Start Exploring</Button>
       <Button>Get a Demo</Button>
+      <Paragraph>
+        Sberify combines the best music experience with an obsessive focus on
+        things you like.
+        <br />
+        It contains a lot of artists, albums, songs, lyrics & even more...
+      </Paragraph>
     </Wrapper>
   </Layout>
 )
