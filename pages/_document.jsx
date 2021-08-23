f5f9e46d59386e9f9dc56695bdc628cbf578745d
Кузнецов Максим Александрович <MAlKuznetsov@sberbank.ru>
2021-08-23 19:09:29 +0300

    [Dev] FOUT fix via preloading TODO: optimize fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,14 +7,14 @@ export default class extends Document {
         return (
             <Html className="dark-mode">
                 <Head>
-                    <link rel="preload" href="/fonts/Archivio/Light.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Archivio/Regular.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Archivio/SemiBold.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Archivio/Bold.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Archivio/ExtraBold.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Roboto/Light.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Roboto/Medium.ttf" as="font" crossOrigin />
-                    <link rel="preload" href="/fonts/Roboto/Regular.ttf" as="font" crossOrigin />
+                    <link rel="preload" href="/fonts/Archivio/Light.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Archivio/Regular.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Archivio/SemiBold.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Archivio/Bold.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Archivio/ExtraBold.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Roboto/Light.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Roboto/Medium.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Roboto/Regular.ttf" as="font" crossOrigin="" />
                 </Head>
                 <body>
                     <script src="/js/noflash.js" />
