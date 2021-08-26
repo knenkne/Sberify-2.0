5d28f46b2f4f5420fa484f9a0bdfd1c339625668
Max Kuznetsov <jztenk@gmail.com>
2021-08-27 01:35:38 +0300

    [Theme] Removing theme button mount hotfix

--- components/button/theme/index.jsx
+++ components/button/theme/index.jsx
@@ -1,22 +1,14 @@
 import { useTheme } from 'next-themes';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 
 import { ThemeButtonStyled } from './styles';
 
 const ThemeButton = () => {
-    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
     const handleClick = useCallback(() => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
     }, [theme]);
-    // When mounted on client, now we can show the UI
-    useEffect(() => setMounted(true), []);
-    // useEffect(() => {
-    //     document.documentElement.classList.toggle('dark-mode', darkModeActive);
-    // }, [darkModeActive]);
-    if (!mounted) {
-        return null;
-    }
+
     return <ThemeButtonStyled onClick={handleClick}>{/* <WaveStyled /> */}</ThemeButtonStyled>;
 };
 
