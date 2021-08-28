cc2c32a4601051a20233e7d6b638892c351b34cf
Max Kuznetsov <jztenk@gmail.com>
2021-08-28 18:28:56 +0300

    [Dev] Album w/ tracks

--- components/album-info/index.jsx
+++ components/album-info/index.jsx
@@ -7,11 +7,20 @@ import {
     AlbumStyled,
     AlbumWrapperStyled,
     CoverWrapperStyled,
+    FeatStyled,
     LinkStyled,
     SubtitleStyled,
-    TitleStyled
+    TitleStyled,
+    TrackArtistStyled,
+    TrackInfoStyled,
+    TrackNameStyled,
+    TrackNumberStyled,
+    TrackPlayButtonStyled,
+    TracksStyled,
+    TrackStyled
 } from './styles';
 
+const FEAT_REG = /\(()(feat|ft)/;
 const months = [
     'January',
     'February',
@@ -35,32 +44,72 @@ const AlbumInfo = (props) => {
     const releaseMonth = months[releseDateParsed.getUTCMonth()];
 
     return (
-        <AlbumWrapperStyled>
-            <AlbumStyled>
-                <CoverWrapperStyled>
-                    <NextImage
-                        src={props.images[1].url}
-                        // TODO: alt
-                        // alt={`${props.name} by ${props.artist}`}
-                        layout="fixed"
-                        width="300"
-                        height="300"
-                        unoptimized
-                    />
-                </CoverWrapperStyled>
-                <AlbumInfoStyled>
-                    <TitleStyled>{props.name}</TitleStyled>
-                    <SubtitleStyled>
-                        {props.artists.map(({ name, id }) => (
-                            <NextLink href={`/artist/${id}`} passHref key={id}>
-                                <LinkStyled>{name} • </LinkStyled>
-                            </NextLink>
-                        ))}
-                        {releaseMonth} {releaseYear}
-                    </SubtitleStyled>
-                </AlbumInfoStyled>
-            </AlbumStyled>
-        </AlbumWrapperStyled>
+        <>
+            <AlbumWrapperStyled>
+                <AlbumStyled>
+                    <CoverWrapperStyled>
+                        <NextImage
+                            src={props.images[1].url}
+                            // TODO: alt
+                            // alt={`${props.name} by ${props.artist}`}
+                            layout="fixed"
+                            width="300"
+                            height="300"
+                            unoptimized
+                        />
+                    </CoverWrapperStyled>
+                    <AlbumInfoStyled>
+                        <TitleStyled>{props.name}</TitleStyled>
+                        <SubtitleStyled>
+                            {props.artists.map(({ name, id }) => (
+                                <>
+                                    <NextLink href={`/artist/${id}`} passHref key={id}>
+                                        <LinkStyled>{name}</LinkStyled>
+                                    </NextLink>
+                                    {` • `}
+                                </>
+                            ))}
+                            {releaseMonth} {releaseYear}
+                        </SubtitleStyled>
+                    </AlbumInfoStyled>
+                </AlbumStyled>
+            </AlbumWrapperStyled>
+            <TracksStyled>
+                {props.tracks.items.map((track, i) => {
+                    const [name] = track.name.split(FEAT_REG);
+                    return (
+                        <TrackStyled key={track.id}>
+                            <TrackPlayButtonStyled>
+                                <svg viewBox="0 0 460.114 460.114">
+                                    <path
+                                        d="M393.538,203.629L102.557,5.543c-9.793-6.666-22.468-7.372-32.94-1.832c-10.472,5.538-17.022,16.413-17.022,28.26v396.173
+			c0,11.846,6.55,22.721,17.022,28.26c10.471,5.539,23.147,4.834,32.94-1.832l290.981-198.087
+			c8.746-5.954,13.98-15.848,13.98-26.428C407.519,219.477,402.285,209.582,393.538,203.629z"
+                                    />
+                                </svg>
+                            </TrackPlayButtonStyled>
+                            <TrackNumberStyled>{i + 1}</TrackNumberStyled>
+                            <TrackInfoStyled>
+                                <TrackNameStyled>{name}</TrackNameStyled>
+                                <TrackArtistStyled>
+                                    {track.artists.map(({ name, id }, i) => (
+                                        <>
+                                            {/* Not a first (main) artist -> It's a feat */}
+                                            {Boolean(i) &&
+                                                (i === 1 ? <FeatStyled> ft. </FeatStyled> : ', ')}
+                                            <NextLink href={`/artist/${id}`} passHref key={id}>
+                                                <LinkStyled>{name}</LinkStyled>
+                                            </NextLink>
+                                        </>
+                                    ))}
+                                </TrackArtistStyled>
+                            </TrackInfoStyled>
+                            <TrackNumberStyled>0:30</TrackNumberStyled>
+                        </TrackStyled>
+                    );
+                })}
+            </TracksStyled>
+        </>
     );
 };
 
--- components/album-info/styles.js
+++ components/album-info/styles.js
@@ -71,3 +71,100 @@ export const LinkStyled = styled.a`
         text-shadow: 0 0 1.4px var(--nav);
     }
 `;
+
+export const TracksStyled = styled.ul`
+    padding: 40px;
+`;
+
+export const TrackNumberStyled = styled.span`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
+    height: 60px;
+    flex-shrink: 0;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: 'Roboto';
+    color: var(--secondary-text);
+`;
+export const TrackInfoStyled = styled.div`
+    flex-grow: 1;
+`;
+
+export const TrackNameStyled = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    font-family: 'Roboto';
+    color: var(--primary-text);
+`;
+
+export const FeatStyled = styled.span`
+    color: var(--primary-text);
+`;
+
+export const TrackArtistStyled = styled.h4`
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--secondary-text);
+`;
+
+export const TrackPlayButtonStyled = styled.button`
+    display: flex;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    left: 0;
+    top: 0;
+    width: 60px;
+    height: 60px;
+    background-color: var(--secondary-BG);
+    opacity: 0;
+    cursor: pointer;
+
+    &:focus {
+        opacity: 1;
+    }
+
+    svg {
+        width: 16px;
+        height: 16px;
+        path {
+            fill: var(--primary-text);
+        }
+    }
+`;
+
+export const TrackStyled = styled.li`
+    position: relative;
+    display: flex;
+    align-items: center;
+    height: 60px;
+    /* background: rgba(255, 255, 255, 0.2); */
+    border-radius: 4px;
+    overflow: hidden;
+
+    &::after {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background: linear-gradient(90deg, var(--primary-brand), var(--secondary-brand));
+    }
+
+    &:hover,
+    &:focus-within {
+        background-color: var(--secondary-BG);
+
+        &::after {
+            content: '';
+        }
+
+        ${TrackPlayButtonStyled} {
+            opacity: 1;
+        }
+    }
+`;
