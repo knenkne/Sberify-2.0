096aa65783355b5d08d2c4237bf5d70a4ed8b6ca
Max Kuznetsov <jztenk@gmail.com>
2021-09-05 02:52:50 +0300

    [Optimization] Removing unnecessary preconnect link

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,6 @@ export default class extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;900&family=Roboto:wght@400;500&display=swap"
                         rel="stylesheet"
