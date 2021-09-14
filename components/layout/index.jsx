14361ce631c935437d24ed0c15c515d986230a71
Max Kuznetsov <jztenk@gmail.com>
2021-09-14 23:52:46 +0300

    [Optimization] svg babel plugin

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -2,6 +2,7 @@ import cx from 'classnames';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 
+import SearchIcon from '../../public/icons/search.svg';
 import { Route } from '../../shared/constants';
 import Link from '../link';
 import Sidebar from '../siderbar';
@@ -26,7 +27,7 @@ const Layout = ({ children, title = 'Sberify 2.0', index }) => {
                                 ))}
                             </nav>
                             <div className="relative w-72 ml-10 h-8">
-                                <svg
+                                <SearchIcon
                                     className={cx(
                                         'absolute',
                                         'top-2',
@@ -36,14 +37,7 @@ const Layout = ({ children, title = 'Sberify 2.0', index }) => {
                                         index ? 'text-[#ffffff]' : 'text-primary',
                                         'fill-current'
                                     )}
-                                    version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 512 512"
-                                    xmlnsXlink="http://www.w3.org/1999/xlink">
-                                    <g>
-                                        <path d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5   S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z" />
-                                    </g>
-                                </svg>
+                                />
                                 <input
                                     className={cx(
                                         'w-full',
