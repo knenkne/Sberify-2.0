fd2969104c9450a13092f6cd1ec22abb6d431215
Max Kuznetsov <jztenk@gmail.com>
2021-09-15 01:30:07 +0300

    [Optimization] Stop propogation on carousel drag

--- components/releases/index.jsx
+++ components/releases/index.jsx
@@ -1,14 +1,29 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402
 import { useEmblaCarousel } from 'embla-carousel/react';
 import NextImage from 'next/image';
 import NextLink from 'next/link';
 import PropTypes from 'prop-types';
+import { useCallback } from 'react';
 
 const Releases = ({ releases }) => {
-    const [emblaRef] = useEmblaCarousel({
+    const [emblaRef, embla] = useEmblaCarousel({
         loop: false,
         align: 'start',
         containScroll: 'keepSnaps'
     });
+
+    // tiny hack for preact to stop propogation while drag the carousel
+    const handleClick = useCallback(
+        (e) => {
+            if (!embla?.clickAllowed()) {
+                e.preventDefault();
+            }
+        },
+        [embla]
+    );
 
     return (
         // TODO: semantic components
@@ -45,7 +60,9 @@ const Releases = ({ releases }) => {
                                 className="embla__slide group relative w-36 h-36 flex flex-col justify-end flex-shrink-0 box-content pr-10"
                                 key={id}>
                                 <NextLink href={`/album/${id}`} passHref>
-                                    <a className="absolute w-36 h-36 shadow-md group-hover:-translate-y-10  group-hover:shadow-xl focus:-translate-y-10 delay-75 duration-300">
+                                    <a
+                                        className="absolute w-36 h-36 shadow-md group-hover:-translate-y-10  group-hover:shadow-xl focus:-translate-y-10 delay-75 duration-300"
+                                        onClick={handleClick}>
                                         <div className="relative w-full h-full rounded overflow-hidden">
                                             <NextImage
                                                 src={coverUrl}
