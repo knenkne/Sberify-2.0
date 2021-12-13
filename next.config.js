79f303f6b07bd930397c6c4acc4f4f82ade4c864
Max Kuznetsov <jztenk@gmail.com>
2021-12-13 19:57:15 +0300

    [Layout] Next Cache

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = {
         topLevelAwait: true
     },
     env: {
-        BASE_URL: process.env.BASE_URL,
         CLIENT_SECRET: process.env.CLIENT_SECRET,
         TOKEN_URL: process.env.TOKEN_URL,
         API_URL: process.env.API_URL,
