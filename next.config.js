93848e6391a08143ed956c95ad3d79559160a074
knenkne <jztenk@gmail.com>
2022-04-18 00:07:43 +0300

    [Hotfix] stop capturing when carousel in progress

--- next.config.js
+++ next.config.js
@@ -18,13 +18,11 @@ module.exports = {
     },
     webpack: (config, { dev, isServer }) => {
         config.experiments = { topLevelAwait: true, layers: true };
-        if (!dev && !isServer) {
-            Object.assign(config.resolve.alias, {
-                react: 'preact/compat',
-                'react-dom/test-utils': 'preact/test-utils',
-                'react-dom': 'preact/compat'
-            });
-        }
+        Object.assign(config.resolve.alias, {
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat'
+        });
 
         return config;
     }
