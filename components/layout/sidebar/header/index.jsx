6f7e988b7d8151fb3c16ee0937e8d32e6b43c46f
knenkne <jztenk@gmail.com>
2023-04-21 01:31:47 +0300

    [Setup] Fix SiderbarHeader's styles

--- components/layout/sidebar/header/index.jsx
+++ components/layout/sidebar/header/index.jsx
@@ -6,7 +6,7 @@ import { SidebarItem } from '../item';
 const SidebarHeader = ({ track }) => {
     return (
         <header className="relative w-72 mb-4 bg-primary  rounded-t-lg shadow-md shadow-black/75">
-            <SidebarItem className="relative flex-wrap px-0">
+            <SidebarItem className="relative flex-wrap !px-0">
                 <Player {...track} />
             </SidebarItem>
         </header>
